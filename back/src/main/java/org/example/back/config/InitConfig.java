a4f4516604f47df7227939f94dbe8a36f2ceb2cc
Aurelien Boets <aurelien.boets59@gmail.com>
2024-04-22 11:30:04 +0200

    add Role in initConfig

--- back/src/main/java/org/example/back/config/InitConfig.java
+++ back/src/main/java/org/example/back/config/InitConfig.java
@@ -3,7 +3,7 @@ package org.example.back.config;
 import org.example.back.entity.JobTitle;
 import org.example.back.entity.Role;
 import org.example.back.entity.User;
-import org.example.back.repository.UserRepository;
+import org.example.back.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,19 +12,22 @@ import org.springframework.stereotype.Component;
 public class InitConfig implements CommandLineRunner {
 
     @Autowired
-    private UserRepository repository;
+    private UserService service;
 
 
     private void thereIsAdmin(){
-        User user=repository.searchUserByRole(Role.ADMIN);
+        User user=service.findUserByRole(Role.ADMIN);
         if(user==null){
             User admin=User.builder().
                     firstname("admin").
                     lastname("admin").
                     jobTitle(JobTitle.ADMIN).
                     email("admin@mail.fr").
-                    password("admin").build();
-            repository.save(admin);
+                    password("admin").
+                    role(Role.ADMIN).
+                    build();
+
+            service.createUser(admin);
         }
     }
     @Override
