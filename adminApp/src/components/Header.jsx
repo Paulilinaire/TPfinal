43d93e351a9593201d579f742c515b9f5d4fc24f
JOSPINRemi <remi.jospin@gmail.com>
2024-04-22 17:29:40 +0200

    création du dashboard

---
+++ adminApp/src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return(
+      <>
+          <header className="header">
+              <img src="src/assets/LOGO.png" alt="test"/>
+              <p>TimeFlow</p>
+              <button>Add employee</button>
+          </header>
+      </>
+  );
+}
+
+export default Header;
--- adminApp/src/main.jsx
+++ adminApp/src/main.jsx
@@ -4,10 +4,12 @@ import "./index.css";
 // import { RouterProvider } from "react-router-dom";
 // import router from "./app-routing.js";
 import LoginForm from "./components/LoginForm.jsx";
+import Dashboard from "./components/Dashboard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    <LoginForm />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <RouterProvider router={router} /> */}
+        {/*<LoginForm />*/}
+        <Dashboard />
+    </React.StrictMode>
 );
