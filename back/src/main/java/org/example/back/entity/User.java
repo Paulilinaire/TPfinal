21b686a344678ba345c5298d67af213a147b8a4b
Aurelien Boets <aurelien.boets59@gmail.com>
2024-04-22 09:38:41 +0200

    add security

--- back/src/main/java/org/example/back/entity/User.java
+++ back/src/main/java/org/example/back/entity/User.java
@@ -2,7 +2,12 @@ package org.example.back.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
@@ -12,7 +17,7 @@ import java.util.List;
 @Getter
 @Setter
 @Builder
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -24,4 +29,34 @@ public class User {
     private String password;
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "user")
     private List<Pointing> pointingList;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- back/src/main/java/org/example/back/repository/UserRepository.java
+++ back/src/main/java/org/example/back/repository/UserRepository.java
@@ -5,7 +5,11 @@ import org.example.back.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     User searchUserByRole(Role role);
+
+    Optional<User> findByEmail(String email);
 }
