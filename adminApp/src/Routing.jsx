0697ba1c7da438850460476f12fd5c0d95a2785f
Aurelien Boets <aurelien.boets59@gmail.com>
2024-04-23 10:52:32 +0200

    add style

--- adminApp/src/Routing.jsx
+++ adminApp/src/Routing.jsx
@@ -1,15 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
-import Test from "./components/Test";
 
 const Routing = createBrowserRouter([
   {
     path: "/",
     element: <LoginForm />,
-  },
-  {
-    path: "/test",
-    element: <Test />,
   },
 ]);
 
--- adminApp/src/components/LoginForm.jsx
+++ adminApp/src/components/LoginForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import authService from "../service/auth-service";
+import logo from "../assets/LOGO.png";
+import background from "../assets/backgroundLogin.png";
 
 const LoginForm = () => {
   const navigate = useNavigate();
@@ -15,73 +17,71 @@ const LoginForm = () => {
       // navigate();
       // Insérer lien page d'accueil
     } catch (e) {
-      setError("Échec de la connexion. Veuillez vérifier vos identifiants.");
+      setError("Incorrect logins");
     }
   };
 
   return (
     <>
-      <div className="flex content-between">
-        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Welcome back
-            </h2>
-            <h3 className="text-center text-1xl leading-9 tracking-tight text-gray-900">
-              Login to continue
+      <header className="flex items-center">
+        <img src={logo} alt="Logo" className="size-36" />
+        <h2 className="text-xl">
+          <span style={{ color: "#FA9746" }}>Time</span>
+          <span className="font-bold">Flow</span>
+        </h2>
+      </header>
+      <main className="flex space-x-2.5 items-center">
+        <div
+          style={{ borderColor: "#B0D0FF" }}
+          className="border-2 rounded-lg ml-5 pl-3 w-5/12 drop-shadow"
+        >
+          <div className="ml-40 mt-28">
+            <h3 style={{ color: "#233863" }} className="font-semibold text-xl">
+              Welcome back !
             </h3>
-            {error && <span>{error}</span>}
+            <h2 style={{ color: "#FA9746" }} className="font-medium m-3	">
+              Login to continue
+            </h2>
           </div>
-          <div className=" sm:mx-auto sm:w-full sm:max-w-sm">
-            <form
-              className="sm:mx-auto sm:w-full sm:max-w-sm"
-              onSubmit={handleLogin}
+          {error && (
+            <div class="p-4 mb-4 ml-40 w-6/12 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400">
+              <span class="font-medium">Error: </span> {error}
+            </div>
+          )}
+          <form onSubmit={handleLogin} className="ml-40 mt-4 flex flex-col">
+            <input
+              type="text"
+              placeholder="Email"
+              value={mail}
+              onChange={(e) => setMail(e.target.value)}
+              style={{
+                borderColor: "#B0D0FF",
+              }}
+              className="input shadow-inner bg-gray-50 border w-8/12 text-sm rounded-3xl p-2.5"
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={{
+                borderColor: "#B0D0FF",
+              }}
+              className="input shadow-inner bg-gray-50 border w-8/12 text-sm rounded-3xl p-2.5 mt-5"
+              required
+            />
+            <button
+              type="submit"
+              style={{ backgroundColor: "#233863" }}
+              class="mt-5 mb-24 w-8/12 px-6 py-3.5 text-base font-medium text-white rounded-lg text-center"
             >
-              <div>
-                <div className="mt-2">
-                  <input
-                    id="mail"
-                    name="mail"
-                    type="mail"
-                    value={mail}
-                    onChange={(e) => setMail(e.target.value)}
-                    required
-                    placeholder="Email"
-                    className="block w-full px-1.5 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="flex items-center justify-between">
-                  <div className="text-sm">
-                    <div className="mt-2">
-                      <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                        className="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  className=" mt-2 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Log in
-                </button>
-              </div>
-            </form>
-          </div>
+              Log in
+            </button>
+          </form>
         </div>
-        <div></div>
-      </div>
+        <img src={background} alt="Background" />
+      </main>
     </>
   );
 };
